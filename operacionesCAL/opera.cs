874d4f0a30b133f04520d6b846b9d9edead003ae
Jesus Pandura <jesmal.flex@hotmail.com>
2020-10-19 17:45:14 -0700

    Agregar archivos de proyecto.

---
+++ operacionesCAL/opera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacionesCAL
+{
+    public class opera
+    {
+        public double div(double n1, double n2)
+        {
+            double d;
+            d = n1 / n2;
+            return d;
+
+        }
+        public double Mult(double n1, double n2)
+        {
+            double d;
+            d = n1 * n2;
+            return d;
+
+        }
+        public double resta(double n1, double n2)
+        {
+            double d;
+            d = n1 - n2;
+            return d;
+
+        }
+        public double sumar(double n1, double n2)
+        {
+            double d;
+            d = n1 + n2;
+            return d;
+
+        }
+
+    }
+}
